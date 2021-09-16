c5292475abfecfb92217277bd192ccd24b052286
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-16 10:42:10 +0300

    fibonacci mob

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -1,3 +1,16 @@
 module.exports = function (num) {
-  // write your code here
+// write your code here
+let arr = [1, 1];
+for (let i = 0; i < num; i++) {
+let last = arr[arr.length - 1];
+let lastButOne = arr[arr.length - 2];
+let sum = last + lastButOne;
+
+if(sum > num) {
+break;
 }
+arr.push(sum); 
+}
+let filter = arr.filter((element) => element % 2 !== 0);
+return filter.reduce((total, element) => total + element, 0);
+} 
